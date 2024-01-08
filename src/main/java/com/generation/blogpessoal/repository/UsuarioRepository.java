6dca11035662ac60fc9035d0b0ca67ef3c813db1
unknown <luiza.kormann@gmail.com>
2024-01-08 00:54:31 -0300

    correção UsuarioRepository

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -8,6 +8,6 @@ import com.generation.blogpessoal.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	public Optional<Usuario> findByUsuario(String usuario);
+    public Optional<Usuario> findByUsuario(String usuario);
 
 }
