872768ac5afc5c5f8f59af8c07a3ea68c0c8dd39
unknown <luiza.kormann@gmail.com>
2024-01-07 23:43:54 -0300

    swagger

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -29,6 +30,7 @@ public class Usuario {
 	@NotNull(message = "O campo nome é obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O campo usuário é obrigatório!")
 	@Email(message = "Por favor, informe um e-mail válido.")
 	private String usuario;
