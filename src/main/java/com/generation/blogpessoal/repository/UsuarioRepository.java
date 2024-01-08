c174a5e2dba8f37ce9ba318fc45d02a29fbdc7bb
unknown <luiza.kormann@gmail.com>
2024-01-08 02:58:23 -0300

    retirada import inutilizado

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -3,7 +3,6 @@ package com.generation.blogpessoal.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Usuario;
 
